807789d707575c16f3381bdf498288a9faf77265
Zann <yan287062@gmail.com>
2026-09-17 12:35:41 +0630

    Fix unescaped apostrophe

--- app/page.tsx
+++ app/page.tsx
@@ -451,7 +451,7 @@ export default function AdminPanel() {
                       <svg className="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                     </div>
                   </div>
-                  <h3 className="text-indigo-100 text-sm font-bold mb-1">Today's Sales</h3>
+                 <h3 className="text-indigo-100 text-sm font-bold mb-1">Today&apos;s Sales</h3>
                   <p className="text-3xl font-black">{stats.todaySales.toLocaleString()} <span className="text-sm font-medium">Ks</span></p>
                 </div>
 
