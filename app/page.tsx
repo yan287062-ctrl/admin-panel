73c2a71d608138e898e725acc311616aa88662ea
Zann <yan287062@gmail.com>
2026-09-20 21:27:27 +0630

    Fix API URL in Dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 import { createClient } from '@supabase/supabase-js';
 import { savePricesToDb } from './actions/supabase';
 
-const supabaseUrl = 'https://painggyishop.vercel.app/api/supabase';
+const supabaseUrl = 'https://admin.painggyishop.cyou/api/supabase';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxlamZoc3V3YWptemlrbXVkbWNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc3NjA4NzUsImV4cCI6MjEwMzMzNjg3NX0.x3EVXbqCmrq0yiGlKI6GrWadKWU9TuXKs5F3w8uJNQA';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
